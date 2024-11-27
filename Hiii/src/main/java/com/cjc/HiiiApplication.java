cb93352d47b79018a9198a591cee83a033e04d76
saurabhanimje <nimjesaurabh1998@gmail.com>
2024-11-27 15:42:38 +0530

    by piyush

---
+++ Hiii/src/main/java/com/cjc/HiiiApplication.java
@@ -0,0 +1,14 @@
+package com.cjc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiiiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hiii");
+		SpringApplication.run(HiiiApplication.class, args);
+	}
+
+}
